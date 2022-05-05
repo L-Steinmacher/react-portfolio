a726d341d7e7d063b29403a9cc3b2531e0d3ea39
Lucas Steinmacher <lucaslsteinmacher@gmail.com>
2022-05-05 14:01:23 -0700

    working on styling sass

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,29 @@ import React from 'react'
 
 function About() {
   return (
-    <div id='about'>
-        About
+    <div id='about' className='container'>
+        <h3>About</h3>
+        <div className='about-body'>
+          <img src='../assets/' />
+          <p>
+            I'm baby tofu pabst ugh street art godard letterpress glossier listicle jean shorts you probably haven't heard of them poutine chartreuse. Food truck wayfarers squid, migas tote bag keffiyeh chicharrones subway tile fashion axe. Authentic sartorial adaptogen, hammock hell of craft beer tbh slow-carb iceland williamsburg mumblecore vape blue bottle waistcoat. Af forage coloring book kogi umami artisan hashtag tbh tofu. Street art chicharrones air plant schlitz roof party thundercats marfa tonx. Drinking vinegar sriracha jianbing VHS.
+            <br/>
+            <br/>
+            Adaptogen gastropub butcher cornhole knausgaard, ugh dreamcatcher cronut hoodie crucifix lomo sartorial kombucha. You probably haven't heard of them umami tumeric schlitz sriracha everyday carry actually disrupt kinfolk blog before they sold out farm-to-table vape austin four dollar toast. Tilde typewriter jianbing, tonx taxidermy marfa pop-up single-origin coffee. Single-origin coffee humblebrag authentic lo-fi palo santo kickstarter. Retro mixtape activated charcoal hella heirloom blue bottle fam sustainable chicharrones la croix.
+            <br/>
+            <br/>
+            Sustainable mustache humblebrag mlkshk banh mi, try-hard iPhone roof party cold-pressed cornhole. Poutine keffiyeh art party venmo pop-up vinyl pour-over humblebrag. Pour-over poutine vexillologist, pop-up cray fanny pack art party selfies tumblr dreamcatcher narwhal hoodie yes plz. Retro yuccie hell of everyday carry direct trade flannel pok pok prism yes plz godard unicorn intelligentsia food truck. Retro post-ironic everyday carry austin sriracha vice godard enamel pin.
+            <br/>
+            <br/>
+            Kombucha raclette sustainable fam copper mug williamsburg. YOLO mlkshk offal, listicle sartorial vinyl ramps woke leggings activated charcoal polaroid selfies lumbersexual. Freegan narwhal literally forage, shabby chic bicycle rights artisan photo booth craft beer chartreuse pabst pour-over. Air plant microdosing squid pok pok tofu tacos yr palo santo mustache semiotics brooklyn kogi authentic tote bag. Live-edge celiac ramps brooklyn palo santo squid freegan truffaut skateboard subway tile butcher 3 wolf moon beard chambray disrupt.
+            <br/>
+            <br/>
+            Pork belly next level pop-up hoodie. Art party lomo 8-bit taiyaki quinoa pickled af. Chia shaman hexagon poke truffaut retro vexillologist DIY prism umami flexitarian small batch iPhone street art dreamcatcher. Adaptogen church-key jianbing offal fixie chicharrones kombucha aesthetic shaman fingerstache. Brooklyn 90's hot chicken butcher raw denim tousled paleo freegan adaptogen truffaut vice man braid chambray. Asymmetrical church-key prism, palo santo pork belly PBR&B messenger bag yuccie twee vinyl aesthetic celiac humblebrag 3 wolf moon. Vinyl cardigan affogato seitan selvage brunch trust fund praxis, man bun offal.
+            <br/>
+            <br/>
+            Dummy text? More like dummy thicc text, amirite?
+          </p>
+        </div>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './css/style.css'
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
